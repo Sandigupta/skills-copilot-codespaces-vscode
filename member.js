23ef1696c093e035ffe3fd7fe48a5ba898e92eb9
Sandigupta <123671335+Sandigupta@users.noreply.github.com>
2024-06-17 20:08:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  const member = {
+    name: 'John',
+    age: 23,
+    skills: ['JavaScript', 'React', 'Node'],
+  };
+
+  return member;
+}
+
+// Activity: Push code to your repository from the codespace 
+// git add member.js
